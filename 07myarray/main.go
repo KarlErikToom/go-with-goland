fd0d49b790c31fdda06e65677acc82dd9d9090af
Karl <toom.karl42@gmail.com>
2023-10-18 15:16:22 +0300

    arrays in golang

---
+++ 07myarray/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to array in golang")
+
+	var fruitList [4]string
+
+	fruitList[0] = "Apple"
+	fruitList[1] = "Tomato"
+	fruitList[3] = "Banana"
+
+	fmt.Println("Fruit list is: ", fruitList)
+	fmt.Println("Fruit list is: ", len(fruitList))
+
+	var vegList = [3]string{"Potato", "beans", "mushroom"}
+
+	fmt.Println("Vegetable list is: ", len(vegList))
+}
