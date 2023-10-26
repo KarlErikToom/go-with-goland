b5322e128f14c79cb559c336332f3fd461324531
Karl <toom.karl42@gmail.com>
2023-10-26 17:18:23 +0300

    Update a course controller in golang

--- 23buildapi/main.go
+++ 23buildapi/main.go
@@ -71,6 +71,7 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 }
 
 func createOneCourse(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Println("Create one course")
 	w.Header().Set("Content-Type", "application/json")
 
@@ -95,4 +96,27 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	courses = append(courses, course)
 	json.NewEncoder(w).Encode(course)
 	return
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// first - grab id from request
+	params := mux.Vars(r)
+
+	// loop, id, remove, add with my ID
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	//TODO: send a response when id is not found
+
 }
