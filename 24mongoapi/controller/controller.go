84c007f29eb3df558e05e3457c0ae53ca5ee1748
Karl <toom.karl42@gmail.com>
2023-10-28 14:25:23 +0300

    update a record in mongodb in golang

--- 24mongoapi/controller/controller.go
+++ 24mongoapi/controller/controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/karleriktoom/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
@@ -32,4 +35,34 @@ func init() {
 	fmt.Println("MongoDB connection success")
 
 	collection = client.Database(dbName).Collection(colName)
+}
+
+//MONGODB helpers -file
+
+// inster 1 record
+func instertOneMovie(movie model.Netflix) {
+	insterted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Insterted one movie in db with id: ", insterted.InsertedID)
+}
+
+// update 1 record
+func updateOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified count:", result.ModifiedCount)
+
 }
